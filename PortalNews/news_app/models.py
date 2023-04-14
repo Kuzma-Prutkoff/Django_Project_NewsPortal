305da53a2af0717a133b32d14f37b2624df10633
Yureg <suxx5@mail.ru>
2023-04-14 17:10:26 +0300

    add some changes for this old files

--- PortalNews/news_app/models.py
+++ PortalNews/news_app/models.py
@@ -7,6 +7,10 @@ class Author(models.Model):
     author_rating = models.IntegerField(default=0)
     author_user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    class Meta: # меняем в админке названия моделей на удобоваримый
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+
     def update_rating(self):
         postRat = self.post_set.aggregate(postRating=Sum('post_rating'))  # берем модель post в ней суммируем поле post_rating
         pRat = 0
@@ -22,6 +26,13 @@ class Author(models.Model):
 
 class Category(models.Model):
     name_category = models.CharField(max_length=64, unique=True)
+
+    def __str__(self): # теперь в БД во вкладке http://127.0.0.1:8000/admin/news_app/post/ названия категорий
+        return self.name_category
+
+    class Meta: # меняем в админке названия моделей на удобоваримый
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
 class Post(models.Model):
     NEWS = 'NW'
@@ -44,10 +55,20 @@ class Post(models.Model):
          self.save()
     def preview(self):
         return f'{self.text[0:123]}...' # простая версия self.text[0:123]} + '...'
+    def __str__(self): # теперь в БД во вкладке http://127.0.0.1:8000/admin/news_app/post/ названия постов
+        return self.title
+    class Meta: # меняем в админке названия моделей на удобоваримый
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta: # меняем в админке названия моделей на удобоваримый
+        verbose_name = 'Категория новости'
+        verbose_name_plural = 'Категории новостей'
 
 class Comment (models.Model):
     comment_post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -62,3 +83,10 @@ class Comment (models.Model):
     def dislike(self):
         self.comment_rating -= 1
         self.save()
+
+    class Meta: # меняем в админке названия моделей на удобоваримый
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self): # теперь в БД во вкладке http://127.0.0.1:8000/admin/news_app/post/ названия коммент
+        return self.comment_text
--- PortalNews/news_app/views.py
+++ PortalNews/news_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+from datetime import datetime
 
-# Create your views here.
+class NewsList(ListView):
+    model = Post
+    ordering = '-date_in'
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_news'] = "Самая Самая свежая новость"
+        return context
+
+class NewsDetail(DetailView):
+    model = Post
+    template_name = 'new.html'
+    context_object_name = 'new'
