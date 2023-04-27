49a708a294e626cb562621eaa0a668626819a565
Yureg <suxx5@mail.ru>
2023-04-27 10:55:18 +0300

    add path for accounts/

--- PortalNews/PortalNews/urls.py
+++ PortalNews/PortalNews/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/', include('news_app.urls')),
+    path("accounts/", include("allauth.urls")), #теперь можно логиниться через яндекс
+
 ]
