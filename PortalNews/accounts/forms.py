a19a34cec74f960d2ed04fba538242b12cdab0d0
Yureg <suxx5@mail.ru>
2023-05-07 13:01:07 +0300

    add CustomSignupForm for new users to email new users

--- PortalNews/accounts/forms.py
+++ PortalNews/accounts/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from allauth.account.forms import SignupForm
-from django.contrib.auth.models import Group
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="Email")
@@ -12,15 +11,9 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ("username", "first_name", "last_name", "email", "password1", "password2",)
 
-# class CustomSignupForm(SignupForm):
-#     def save(self, request):
-#         user = super().save(request)
-#         group_for_all = Group.objects.get(name="BoTaNy")
-#         user.groups.add(group_for_all)
-#         return user
-
 # При регистрации нвого юзера ему придет на почту письмо, либо текстом-либо html кодом(если поддерживается)
 from django.core.mail import EmailMultiAlternatives
+
 class CustomSignupForm(SignupForm):
     def save(self, request):
         user = super().save(request)
@@ -34,21 +27,3 @@ class CustomSignupForm(SignupForm):
         msg.attach_alternative(html, "text/html")
         msg.send()
         return user
-
-# письмо менеджеру о регистрации нового юзера в сеттинге настройки почт манагеров
-# from allauth.account.forms import SignupForm
-# from django.core.mail import mail_managers
-# class CustomSignupForm(SignupForm):
-#     def save(self, request):
-#         user = super().save(request)
-#         mail_managers(subject='Новый пользователь!', message=f'Пользователь {user.username} зарегистрировался на сайте.')
-#         return user
-
-# письмо админу о регистрации нового юзера в сеттинге настройки почт админов
-# from allauth.account.forms import SignupForm
-# from django.core.mail import mail_admins
-# class CustomSignupForm(SignupForm):
-#     def save(self, request):
-#         user = super().save(request)
-#         mail_admins(subject='Новый пользователь!', message=f'Пользователь {user.username} зарегистрировался на сайте.')
-#         return user
