6d086d660a0af3873ade8ae9b0962b403075d9cf
Yureg <suxx5@mail.ru>
2023-05-07 11:23:37 +0300

    add field post_category

--- PortalNews/news_app/forms.py
+++ PortalNews/news_app/forms.py
@@ -4,5 +4,5 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['author', 'title', 'text']
+        fields = ['author', 'title', 'text', 'post_category']
 
