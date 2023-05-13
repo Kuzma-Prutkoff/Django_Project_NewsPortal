1c09f2342e432cff41eb30cf74a4f711f7eff654
Yureg <suxx5@mail.ru>
2023-05-13 12:19:22 +0300

    signals OFF

--- PortalNews/news_app/apps.py
+++ PortalNews/news_app/apps.py
@@ -1,10 +1,10 @@
-from django.apps import AppConfig
-
-class NewsAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'news_app'
-    def ready(self):
-        from . import signals  # выполнение модуля -> регистрация сигналов. Из вебинара import news_app.signals
-                                 # и в сеттинге заменить news_app на INSTALLED_APPS = [news_app.apps.NewsConfig
-
-
+# from django.apps import AppConfig
+#
+# class NewsAppConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'news_app'
+#     def ready(self):
+#         from . import signals  # выполнение модуля -> регистрация сигналов. Из вебинара import news_app.signals
+#                                  # и в сеттинге заменить news_app на INSTALLED_APPS = [news_app.apps.NewsConfig
+#
+#
