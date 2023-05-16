aaa723ddcbe7ce433b0d1ae953c4b144e2674254
Yureg <suxx5@mail.ru>
2023-05-16 13:34:46 +0300

    refresh

--- PortalNews/news_app/forms.py
+++ PortalNews/news_app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Post
 
-class PostForm(forms.ModelForm):
+class PostForm(forms.ModelForm): # какие поля используются при создании поста
     class Meta:
         model = Post
         fields = ['author', 'title', 'text', 'post_category']
