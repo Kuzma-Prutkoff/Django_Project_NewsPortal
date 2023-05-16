449ef690a09322c14bfd93060b03c493fc4d19b3
Yureg <suxx5@mail.ru>
2023-05-16 13:43:21 +0300

    refresh

--- PortalNews/news_app/signals.py
+++ PortalNews/news_app/signals.py
@@ -1,9 +1,6 @@
-from django.db.models.signals import m2m_changed, post_save
-from django.template.loader import render_to_string
-from django.conf import settings
-from .models import PostCategory, Post
+from django.db.models.signals import m2m_changed
+from .models import PostCategory
 from django.dispatch import receiver
-from django.core.mail import EmailMultiAlternatives
 from .tasks import task_about_new_post
 
 #запускаем сигнал таске ( task_about_new_post().delay() ) на отправку письма о новом посте подписантам категории
@@ -18,6 +15,3 @@ def notify_new_post(sender, instance, **kwargs):
             subscribers_emails += [s.email for s in subscribers]  #список почт подписчиков
         task_about_new_post.delay(instance.preview(), instance.pk, instance.title, subscribers_emails)
 
-
-#instance.title, instance.text, instance.date_in, instance.news_or_article, instance.post_category
-#'author', 'title', 'text', 'post_category'
