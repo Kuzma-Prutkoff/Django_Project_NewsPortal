f9c50859c1c5eb524665a950466accfbd2646bc2
Yureg <suxx5@mail.ru>
2023-05-16 13:35:32 +0300

    refresh

--- PortalNews/news_app/apps.py
+++ PortalNews/news_app/apps.py
@@ -3,8 +3,8 @@ from django.apps import AppConfig
 class NewsAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'news_app'
-#     def ready(self):
-#         from . import signals  # выполнение модуля -> регистрация сигналов. Из вебинара import news_app.signals
+    def ready(self):
+        from . import signals  # выполнение модуля -> регистрация сигналов. Из вебинара import news_app.signals
 #                                  # и в сеттинге заменить news_app на INSTALLED_APPS = [news_app.apps.NewsConfig
 #
 #
